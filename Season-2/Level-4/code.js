832056d213c1b939959b58eb6bbc1a60bb3737b7
Johannes Møgster <johannes.mogster@mojotech.no>
2024-02-26 20:56:30 +0000

    Add rate limiter to API requests

--- Season-2/Level-4/code.js
+++ Season-2/Level-4/code.js
@@ -18,6 +18,16 @@ const fs = require("fs");
 const { exec } = require("node:child_process");
 const app = express();
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 app.use(bodyParser.json());
 app.use(bodyParser.text({ type: "application/xml" }));
 
